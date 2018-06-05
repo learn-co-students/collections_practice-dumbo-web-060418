ea5e409f6eea4e36c0c43c3c19636fd09bbccc4f
mark sauer-utley <mark.utley.713@gmail.com>
2018-06-05 07:59:18 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,40 @@
+def sort_array_asc arr
+  arr.sort
+end
 
+def sort_array_desc arr
+  arr.sort.reverse
+end
+
+def sort_array_char_count arr
+  arr.sort_by { |x| x.length }
+end
+
+def swap_elements arr
+  arr[1], arr[2] = arr[2], arr[1]
+  arr
+end
+
+def reverse_array arr
+  arr.reverse
+end
+
+def kesha_maker arr
+  arr.map do |name|
+    name = name.gsub(name[2], "$")
+  end
+end
+
+def find_a arr
+  arr.select { |word| word.start_with?("a") }
+end
+
+def sum_array arr
+  arr.reduce(:+)
+end
+
+def add_s arr
+  arr.map do |word|
+    arr.index(word) == 1 ? word : word << "s"
+  end
+end
